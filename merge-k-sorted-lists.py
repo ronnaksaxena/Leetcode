78ad7183b53ab841475829efb82660c37bc4e73e
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2020-11-24 11:21:03 -0500

    Update merge-k-sorted-lists.py

--- merge-k-sorted-lists.py
+++ merge-k-sorted-lists.py
@@ -3,9 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-​
-from queue import PriorityQueue
-​
+
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
         head = point = ListNode(0)
