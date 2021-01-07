3beb6651a43964b2831b5b73a53c2a4556ba3217
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2021-01-07 12:46:07 -0500

    working commit - Created using LeetHub

---
+++ 2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        MOD = 10**9 + 7
+        moves = [[4,6],[6,8],[7,9],[4,8],[3,9,0],[],
+                     [1,7,0],[2,6],[1,3],[2,4]]
+​
+        dp = [1] * 10
+        for hops in range(n-1):
+            dp2 = [0] * 10
+            for node, count in enumerate(dp):
+                for nei in moves[node]:
+                    dp2[nei] += count
+                    dp2[nei] %= MOD
+            dp = dp2
+        return sum(dp) % MOD
+            
+        
+        
