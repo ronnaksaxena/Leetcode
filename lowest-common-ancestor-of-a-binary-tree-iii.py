63fdcb4655e2799455f4fde0cffb73359112cb90
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2021-09-20 08:20:24 +0200

    working commit - Created using LeetHub

---
+++ lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+​
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        rents = set()
+        
+        while p:
+            rents.add(p)
+            p = p.parent
+            
+        while q:
+            if q in rents:
+                return q
+            else:
+                q = q.parent
+                
+        return None
+        
