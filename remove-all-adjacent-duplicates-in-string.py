53ca6a026e746565e112e283755ee14091b56891
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2021-09-23 18:00:23 +0200

    working commit - Created using LeetHub

---
+++ remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, S: str) -> str:
+        # generate 26 possible duplicates
+        duplicates = {2 * ch for ch in ascii_lowercase}
+        
+        prev_length = -1
+        while prev_length != len(S):
+            prev_length = len(S)
+            for d in duplicates:
+                S = S.replace(d, '')
+                
+        return S
+        
+        
