7a697de135c2c38e5deef475df9a31321e7a7729
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2022-02-08 15:38:31 +0000

    working commit - Created using LeetHub

---
+++ count-substrings-with-only-one-distinct-letter.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countLetters(self, s: str) -> int:
+        count = 1
+        total = 1
+        
+        for i in range(1, len(s)):
+            if s[i] == s[i-1]:
+                count += 1
+            else:
+                count = 1
+            total += count
+            
+        return total
+        
