280ceabb45009fd3673d9d37ad8ee17384e03c42
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2022-04-10 18:50:09 +0100

    working commit - Created using LeetHub

---
+++ baseball-game.py
@@ -0,0 +1,17 @@
+class Solution:
+    def calPoints(self, ops: List[str]) -> int:
+        stack = []
+        
+        for x in ops:
+            if x == 'C':
+                stack.pop()    
+            elif x == '+':
+                stack.append(stack[-1]+stack[-2])
+            elif x == 'D':
+                stack.append(stack[-1]*2)
+            else:
+                stack.append(int(x))
+                
+                
+        return sum(stack)
+        
