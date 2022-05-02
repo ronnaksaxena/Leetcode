94052427b3c03e706f378880d1c68ddba0a833f4
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2022-05-02 19:59:31 +0100

    working commit - Created using LeetHub

---
+++ sort-array-by-parity.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] %2 == 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i += 1
+                
+        return nums
+        
