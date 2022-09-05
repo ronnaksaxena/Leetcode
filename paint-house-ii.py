5ed998bf66e0c8ff52fbef2cef18f01eb7d778a4
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2022-09-05 16:17:12 -0700

    working commit - Created using LeetHub

---
+++ paint-house-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:
+​
+        n = len(costs)
+        if n == 0: return 0
+        k = len(costs[0])
+​
+        for house in range(1, n):
+            # Find the colors with the minimum and second to minimum
+            # in the previous row.
+            min_color = second_min_color = None
+            for color in range(k):
+                cost = costs[house - 1][color]
+                if min_color is None or cost < costs[house - 1][min_color]:
+                    second_min_color = min_color
+                    min_color = color
+                elif second_min_color is None or cost < costs[house - 1][second_min_color]:
+                    second_min_color = color
+            # And now update the costs for the current row.
+            for color in range(k):
+                if color == min_color:
+                    costs[house][color] += costs[house - 1][second_min_color]
+                else:
+                    costs[house][color] += costs[house - 1][min_color]
+​
