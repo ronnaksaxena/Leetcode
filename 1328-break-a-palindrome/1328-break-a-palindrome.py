17ff13f54fe1b439c5877498ba18ca62ac3f2299
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-01-10 12:32:44 -0500

    Time: 31 ms (85.85%), Space: 13.9 MB (13.40%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,20 +1,21 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
         '''
-        abba
-        aaab
+        Time: O(n)
+        Space: O(1)
         '''
         # Edge case can't make it not a palindrome
         if len(palindrome) == 1:
             return ''
-        
+        # Find first char that is not a
         for i, c in enumerate(palindrome):
             if c == 'a':
                 continue
+            # Ignore middle element since it will remain a palendrome
             elif i == len(palindrome) // 2:
                 continue
-            
+            # Replace with a
             return palindrome[:i] + 'a' + palindrome[i+1:]
-        
+        # All elements are a so change last one to b
         return palindrome[:-1] + 'b'
         
