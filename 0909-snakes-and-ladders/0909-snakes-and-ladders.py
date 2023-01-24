d4780f33a63533a8c31d958c19f4eea9a004c248
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-01-24 11:37:27 -0500

    Time: 204 ms (40.29%), Space: 14.4 MB (7.02%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.py
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.py
@@ -2,17 +2,19 @@ class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         totalCells = len(board) * len(board[0])
         ROWS, COLS = len(board), len(board[0])
+        # Returns the board index of the cell
         def findIndex(cell):
             nonlocal ROWS
             nonlocal COLS
             row = (ROWS-1) - ((cell-1) // COLS)
-            # Increasing row if odd, decreasing if even, 1-indexed
+            # Increasing row if even, decreasing if odd, 0-indexed
             col = (cell-1)%COLS if (ROWS-1-row) % 2 == 0 else (COLS-1) - (cell-1)%COLS
             return [row, col]
         
         destination = totalCells
         visited = {1}  # cell
         q = collections.deque([(1, 0)])  # (cell, moves)
+        # BFS
         while q:
             cell, moves = q.popleft()
             print(cell, moves)
