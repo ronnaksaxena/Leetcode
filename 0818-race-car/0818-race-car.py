fcd30bb44547c93b37aa7c93576d9cd4dddfa7bf
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-01-29 20:44:56 -0500

    Time: 47 ms (84.18%), Space: 15.8 MB (48.43%) - LeetHub

---
+++ 0818-race-car/0818-race-car.py
@@ -0,0 +1,36 @@
+class Solution:
+    def racecar(self, target: int) -> int:
+        
+        moves = 0
+        q = collections.deque([(0, 1)]) # (position, speed)
+        visited = {(0, 0)}
+        # BFS
+        while q:
+            
+            for _ in range(len(q)):
+                position, speed = q.popleft()
+
+                #check if reached target
+                if position == target:
+                    return moves
+                #already seen
+                if (position, speed) in visited:
+                    continue
+
+                visited.add((position,speed))
+
+                #try to accelerate
+                q.append((position+speed, speed*2))
+
+                #try to reverse under 2 conditions:
+                #1: car is going foreward and you will pass target
+                #2: car is going in reverse and is before target
+                if (speed > 0 and position+speed > target) or (speed < 0 and position+speed < target):
+                    #update speed according to conditions
+                    speed = -1 if speed > 0 else 1
+                    #do not change positions
+                    q.append((position,speed))
+            moves += 1
+            
+        return -1
+                
