5c3c7978e4a99e01b21538728f20ff9d1ebf2049
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-02-05 13:45:56 -0500

    Time: 138 ms (67.19%), Space: 15.3 MB (27.00%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        '''
+        s = "cbaebabacd"
+             l
+                r
+        p = "abc"
+        {
+        a: 0
+        b: 1
+        c: 1
+        }
+        output = [0, 6]
+        '''
+        # populate letters count
+        letters = {chr(i): 0 for i in range(ord('a'), ord('z')+1)}
+        for c in p:
+            letters[c] += 1
+        l, r = 0, 0
+        output = []
+        while r < len(s):
+            if letters[s[r]] > 0:
+                letters[s[r]] -= 1
+                # Found anagram
+                if sum(letters.values()) == 0:
+                    output.append(l)
+                r += 1
+            elif l < r:
+                letters[s[l]] += 1
+                l += 1
+            else:
+                l += 1
+                r += 1
+        return output
+                    
+                    
+        
