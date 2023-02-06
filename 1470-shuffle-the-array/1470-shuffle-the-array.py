34841c0a9ea2f95d35becbd04fc7c58f738557ec
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-02-06 12:02:47 -0500

    Time: 78 ms (28.06%), Space: 14.2 MB (33.52%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        '''
+        [2,5,1,3,4,7]
+         0,1,2,3,4,5
+        [2,3,5,4,1,7]
+         0,3,1,4,5,2
+        [2,3,5,4,1,7]
+             i
+             x
+                   y
+        ''' 
+        
+        output = [0 for _ in range(2*n)]
+        x, y = 0, n
+        
+        for i in range(0, 2 * n, 2):
+            output[i] = nums[x]
+            output[i+1] = nums[y]
+            x += 1
+            y += 1
+            
+        return output
+        
