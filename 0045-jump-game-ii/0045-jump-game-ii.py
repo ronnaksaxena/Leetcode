603b608e791fcca0cb3f62c3ecbd93a14c5cc5dd
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-02-08 17:11:47 -0500

    Time: 134 ms (73.19%), Space: 14.9 MB (89.44%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,47 +1,23 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         '''
-        nums = 2,
-              / \
-             3   1
-             /\
-            1  4
-        Time: O(n)
-        Space: O(1)
-        PSUEDOCODE
-        jumps = 0
-        start, end = 0, 0
-        furthestJump = 0
-        while end < len(nums)-1:
-            for i in range(start, end+1):
-                furthestJump = max(furthestJump, i + nums[i])
-            jumps += 1
-            start = end + 1
-            end = furthestJump
-        return jumps
-        
         [2,3,1,1,4]
                s
-                  e
-                  f
-         
-         j = 1
+                 e
         
+        jumps = 2
         '''
         
-        jumps = 0
-        furthestJump = 0
         start, end = 0, 0
-        # Want to stop when window includes last element
-        while end < len(nums)-1:
+        jumps = 0
+        
+        while end < len(nums) - 1:
+            furthestJump = end
             for i in range(start, end+1):
-                furthestJump = max(furthestJump, i + nums[i])
-            jumps += 1
+                furthestJump = max(furthestJump, nums[i] + i)
             start = end + 1
             end = furthestJump
+            jumps += 1
+            
         return jumps
-        
-        
-        
-        
         
