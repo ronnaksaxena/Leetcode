ff326a8faef050e4b84faf74c80d200995f0e9aa
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-02-14 23:48:54 -0500

    Time: 301 ms (82.17%), Space: 14.9 MB (85.98%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,25 +1,25 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        kDigits = [int(d) for d in str(k)]
-        output = collections.deque([0 for _ in range(max(len(kDigits),len(num)))])
-        
-        
+        i = len(num) - 1
         carry = 0
-        kPtr = len(kDigits)-1
-        i = len(num) -1
-        j = len(output) - 1
-        while i >= 0 or kPtr >= 0:
-            total = (num[i] if i >= 0 else 0) + (kDigits[kPtr] if kPtr >= 0 else 0) + carry
-            carry = total// 10
-            digit = total%10
-            output[j] = digit
-            kPtr -= 1
+        result = []
+
+        while i >= 0 or k > 0:
+            digit1 = num[i] if i >= 0 else 0
+            digit2 = k % 10 if k > 0 else 0
+            sum = digit1 + digit2 + carry
+            carry = sum // 10
+            result.append(sum % 10)
             i -= 1
-            j -= 1
+            k //= 10
+
         if carry > 0:
-            carryDigits = [int(d) for d in str(carry)]
-            while carryDigits:
-                output.appendleft(carryDigits.pop())
-            
-        return output
+            result.append(carry)
+
+        result.reverse()
+        return result
+
+
+
+
         
