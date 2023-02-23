3b310c1d17cc9fac51567303d3325471e3720cb1
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-02-23 14:02:00 -0500

    Time: 91 ms (16.06%), Space: 13.8 MB (50.04%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,4 +1,26 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x) == str(x)[::-1]
+        # if neg not palnedrome
+        if x < 0:
+            return False
+        def getDigitPow(num):
+            res = 1
+            while num:
+                num = num // 10
+                res *= 10
+            return res // 10 #off by one
+        
+        left, right = getDigitPow(x), 10
+        while left >= right:
+            lDigit, rDigit = (x // left)%10, (x % right) // (right // 10)
+            # print(left, right, lDigit, rDigit)
+            if lDigit != rDigit:
+                return False
+            
+            left = left//10
+            right *= 10
+            
+        return True
+            
+            
         
