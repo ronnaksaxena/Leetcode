b3ce0e60d2d638505aac98f5b7747c90129abc50
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-04-05 19:48:27 -0400

    Time: 108 ms (37.13%), Space: 15.4 MB (65.52%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,27 +1,40 @@
+class UF:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1 for _ in range(n)]
+    def find(self, i):
+        while i != self.par[i]:
+            self.par[i] = self.par[self.par[i]]
+            i = self.par[i]
+        return i
+    
+    def union(self, n1, n2):
+        p1, p2 = self.find(n1), self.find(n2)
+        if p1 == p2:
+            return True
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+            self.rank[p1] += self.rank[p2]
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += self.rank[p1]
+        return True
+        
+
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        # Build undirected graph
-        graph = collections.defaultdict(list)
-        for a, b in edges:
-            graph[a].append(b)
-            graph[b].append(a)
-        visited = set()
-        componets = 0
-        # BFS on all components
+        uf = UF(n)
+        for v1, v2 in edges:
+            uf.union(v1, v2)
+        
+        parent = set()
         for i in range(n):
-            if i not in visited:
-                componets += 1
-                q = collections.deque([i])
-                visited.add(i)
-                while q:
-                    cur = q.popleft()
-                    for nei in graph[cur]:
-                        if nei not in visited:
-                            visited.add(nei)
-                            q.append(nei)
-                            
-        return componets
-# Time O(E+V) where E is edges and V is nodes
-# Space O(V) where V is vertices
-                            
+            parent.add(uf.find(i))
+        return len(parent)
+        
+        
+        
+        
+        
+        
         
