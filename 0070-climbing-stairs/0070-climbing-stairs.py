fd049834a4718a3a52fbf5d098ebe7ce925a63d7
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-04-06 10:58:34 -0400

    Time: 19 ms (99.41%), Space: 13.8 MB (44.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,18 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         '''
-        1 2 3 5 8 13 20 28
-        1 2 3 4 5 6 7 8
-        
-        fib = 3
-        
+        Time: O(n)
+        Space: O(1)
         '''
         if n == 1 or n == 2:
             return n
         
         first, second = 1, 2
+        # n + 1 becuase want to go to the nth fib number
         for _ in range(3, n+1):
             first, second = second, first + second
-            
+        # On last iteration second will store calculated nth fib number
         return second
         
