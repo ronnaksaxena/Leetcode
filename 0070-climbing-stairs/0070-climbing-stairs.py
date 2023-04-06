565a7f60e1bcdad975e3d8f3f9a4be04dba3ce76
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-04-06 10:57:20 -0400

    Time: 27 ms (86.58%), Space: 13.8 MB (93.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,12 +1,18 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        '''
+        1 2 3 5 8 13 20 28
+        1 2 3 4 5 6 7 8
+        
+        fib = 3
+        
+        '''
         if n == 1 or n == 2:
             return n
-        first, second = 1, 2
         
+        first, second = 1, 2
         for _ in range(3, n+1):
-            second, first = first + second, second
+            first, second = second, first + second
             
         return second
-            
         
