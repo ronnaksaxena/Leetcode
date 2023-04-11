14f6eb890c97d827296df9ef5fdb378c65e38390
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-04-11 11:07:05 -0400

    Time: 36 ms (40.95%), Space: 13.8 MB (95.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,10 +2,17 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
         DP = [0 for _ in range(n+1)]
+        # Base case if not houses max profit is 0
+        # Base case: if 1 house that value is max profit
         DP[0], DP[1] = 0, nums[0]
-        
+        # Iterate from 2
         for i in range(2, len(DP)):
+            # DP[i] = max(not robbing this house DP[i-1], or robbing this house DP[i-2] + nums[i])
             DP[i] = max(DP[i-2] + nums[i-1], DP[i-1])
         
         return DP[-1]
+    '''
+    Time: O(n)
+    Space: O(n)
+    '''
         
