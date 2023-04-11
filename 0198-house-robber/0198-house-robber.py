df565cb152c59b291a63ccac18bd3d1013d44333
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-04-11 11:05:21 -0400

    Time: 29 ms (83.90%), Space: 13.7 MB (95.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # Edge Case
-        if len(nums) <= 2:
-            return max(nums)
         n = len(nums)
         DP = [0 for _ in range(n+1)]
         DP[0], DP[1] = 0, nums[0]
         
-        for i in range(1, len(DP)):
+        for i in range(2, len(DP)):
             DP[i] = max(DP[i-2] + nums[i-1], DP[i-1])
         
         return DP[-1]
