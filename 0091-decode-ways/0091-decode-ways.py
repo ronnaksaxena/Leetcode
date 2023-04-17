cfdf1587f3c4c97e54cbd34a06ae281e91750af4
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-04-17 16:44:02 -0400

    Time: 37 ms (39.11%), Space: 13.9 MB (35.61%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = [0] * (len(s)+1)
+        # Base cases
+        # Empty string is 1 combo
+        dp[0] = 1
+        # First digit has 1 combo unless leading zero
+        dp[1] = 1 if s[0] != '0' else 0
+        
+        for i in range(2, len(dp)):
+            # No leading 0
+            if s[i-1] != '0':
+                dp[i] = dp[i-1]
+                
+            twoDig = int(s[i-2:i])
+            if 10 <= twoDig <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[-1]
+                
