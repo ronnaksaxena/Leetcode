0606d9797d95ba253232ba646cb5128651b036b0
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-04-17 16:49:00 -0400

    Time: 24 ms (97.73%), Space: 13.9 MB (35.61%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,12 +1,19 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
+        '''
+        1 1 1 0 6
+DP = [1 1 2 2 2 2]
+Time: O(n)
+Space: O(n)
+        
+        '''
         dp = [0] * (len(s)+1)
         # Base cases
         # Empty string is 1 combo
         dp[0] = 1
         # First digit has 1 combo unless leading zero
         dp[1] = 1 if s[0] != '0' else 0
-        
+        # Looking at s[i-1] since DP is 0 indexed
         for i in range(2, len(dp)):
             # No leading 0
             if s[i-1] != '0':
