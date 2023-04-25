ad55575e913549ade498f24f44c7ca544d856064
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-04-25 14:11:03 -0400

    Time: 615 ms (92.73%), Space: 137 MB (53.64%) - LeetHub

---
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -0,0 +1,36 @@
+class Solution:
+    def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> List[int]:
+        n = len(workers)
+        output = [None for _ in range(n)]
+        # to get closest bike paris
+        pq = []
+        # populate with remaining bike pairs
+        workerBikePairs = []
+        
+        # PQ with (distance, worker idx, bike idx)
+        distances = []
+        for wI, (wX, wY) in enumerate(workers):
+            curPairs = []
+            for bI, (bX, bY) in enumerate(bikes):
+                d = abs(bX-wX) + abs(bY-wY)
+                curPairs.append((d, wI, bI))
+            curPairs.sort(reverse=True)
+            heapq.heappush(pq, curPairs.pop())
+            workerBikePairs.append(curPairs)
+        # seen for bikes
+        seenBikes = set()
+        while pq:
+            _, worker, bike = heapq.heappop(pq)
+            if bike not in seenBikes:
+                output[worker] = bike
+                seenBikes.add(bike)
+            else:
+                _, _, nextClosest = workerBikePairs[worker].pop()
+                wX, wY = workers[worker]
+                bX, bY = bikes[nextClosest]
+                d = abs(bX-wX) + abs(bY-wY)
+                heapq.heappush(pq, (d, worker, nextClosest))
+        
+            
+        return output
+        
