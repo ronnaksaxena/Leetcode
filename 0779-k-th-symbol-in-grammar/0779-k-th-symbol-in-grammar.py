3ec8e9ced2e1f5d4b01de64d28754f41c86d51ae
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-10-24 22:04:55 -0400

    Time: 36 ms (73.58%), Space: 16.4 MB (17.91%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,18 @@
+class Solution:
+    def recursion(self, n: int, k: int) -> int:
+        # First row will only have one symbol '0'.
+        if n == 1:
+            return 0
+
+        total_elements = 2 ** (n - 1)
+        half_elements = total_elements // 2
+
+        # If the target is present in the right half, we switch to the respective left half symbol.
+        if k > half_elements:
+            return 1 - self.recursion(n, k - half_elements)
+
+        # Otherwise, we switch to the previous row.
+        return self.recursion(n - 1, k)
+
+    def kthGrammar(self, n: int, k: int) -> int:
+        return self.recursion(n, k)
