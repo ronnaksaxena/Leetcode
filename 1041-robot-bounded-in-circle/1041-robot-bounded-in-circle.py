15f9fae43af2d075238e4c6eee1a3671566c593d
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-11-17 23:05:54 +0530

    Time: 38 ms (57.17%), Space: 16.3 MB (11.24%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        x, y = 0, 0
+        direc = 0 # 0 is n, 1 is r, 2 is s, 3 is l. => %4
+        # Simulate
+        for i in instructions:
+            if i == 'G':
+                if direc == 0:
+                    y += 1
+                elif direc == 1:
+                    x += 1
+                elif direc == 2:
+                    y -= 1
+                else:
+                    x -= 1
+            elif i == 'L':
+                direc = (direc-1)%4
+            else:
+                # R
+                direc = (direc+1)%4
+        # Not a limit cycle
+        return (x == 0 and y == 0) or direc != 0
+        
