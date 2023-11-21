47ff8743e9304aea760803bf9a1359b62d4a376d
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-11-21 23:30:22 +0530

    Time: 45 ms (15.77%), Space: 16.3 MB (13.00%) - LeetHub

--- 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.py
@@ -3,23 +3,24 @@ class Solution:
         x, y = 0, 0
         direc = 0 # 0 is n, 1 is r, 2 is s, 3 is l. => %4
         # Simulate
-        for i in instructions:
-            if i == 'G':
-                if direc == 0:
-                    y += 1
-                elif direc == 1:
-                    x += 1
-                elif direc == 2:
-                    y -= 1
+        for _ in range(4):
+            for i in instructions:
+                if i == 'G':
+                    if direc == 0:
+                        y += 1
+                    elif direc == 1:
+                        x += 1
+                    elif direc == 2:
+                        y -= 1
+                    else:
+                        x -= 1
+                elif i == 'L':
+                    direc = (direc-1)%4
                 else:
-                    x -= 1
-            elif i == 'L':
-                direc = (direc-1)%4
-            else:
-                # R
-                direc = (direc+1)%4
+                    # R
+                    direc = (direc+1)%4
         # Not a limit cycle
-        return (x == 0 and y == 0) or direc != 0
+        return x == 0 and y == 0
 '''
 Time: O(n)
 Space: O(1)
