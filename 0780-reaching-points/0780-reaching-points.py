e5b2701bee7497fae8ecc1aeed0ed3e6665817d5
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-11-21 23:18:49 +0530

    Time: 40 ms (42.98%), Space: 16.4 MB (19.07%) - LeetHub

---
+++ 0780-reaching-points/0780-reaching-points.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reachingPoints(self, sx, sy, tx, ty):
+        while tx >= sx and ty >= sy:
+            if tx == ty:
+                break
+            elif tx > ty:
+                if ty > sy:
+                    tx %= ty
+                else:
+                    return (tx - sx) % ty == 0
+            else:
+                if tx > sx:
+                    ty %= tx
+                else:
+                    return (ty - sy) % tx == 0
+
+        return tx == sx and ty == sy
