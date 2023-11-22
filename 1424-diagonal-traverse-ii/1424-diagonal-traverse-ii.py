f413e9e848e0cc609939a2c7490fc9189a4f20e5
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-11-22 19:19:05 +0530

    Time: 765 ms (89.45%), Space: 37 MB (90.90%) - LeetHub

--- 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
+++ 1424-diagonal-traverse-ii/1424-diagonal-traverse-ii.py
@@ -1,17 +1,15 @@
 class Solution:
     def findDiagonalOrder(self, nums: List[List[int]]) -> List[int]:
         ans = []
-        q = collections.deque([(0,0)])
-        visited = set()
-        while q:
-            curR, curC = q.popleft()
-            ans.append(nums[curR][curC])
+        queue = collections.deque([(0,0)])
+        while queue:
+            row, col = queue.popleft()
+            ans.append(nums[row][col])
             
-            if curR + 1 < len(nums) and curC < len(nums[curR+1]) and (curR+1,curC) not in visited:
-                q.append((curR+1, curC))
-                visited.add((curR+1, curC))
-            if curC + 1 < len(nums[curR]) and (curR, curC+1) not in visited:
-                q.append((curR, curC+1))
-                visited.add((curR, curC+1))
+            if col == 0 and row + 1 < len(nums):
+                queue.append((row + 1, col))
+                
+            if col + 1 < len(nums[row]):
+                queue.append((row, col + 1))
         return ans
         
