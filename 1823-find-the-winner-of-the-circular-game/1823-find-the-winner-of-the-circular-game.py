53b7c28ddfd853dfd37415278ef0f631808df834
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-11-27 02:42:09 +0530

    Time: 47 ms (48.38%), Space: 16.4 MB (41.88%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,15 +1,9 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        players = [i for i in range(1, n+1)]
-        lastPlayer = 0
-        while len(players) > 1:
-            removedPlayer = lastPlayer
-            for _ in range(k-1):
-                removedPlayer = (removedPlayer + 1) % len(players)
-            # set to next player before removing from list
-            players.remove(players[removedPlayer])
-            lastPlayer = removedPlayer%len(players)
-        return players[0]
-                
-            
+        l = list(range(1,n+1))
+        i = 0
+        while len(l) > 1:
+            i = (i + k - 1)%len(l)
+            l.pop(i)
+        return l[0]
         
