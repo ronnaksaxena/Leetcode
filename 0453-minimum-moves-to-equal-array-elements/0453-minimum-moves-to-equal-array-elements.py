bf81f59a27207f3f05e43792f7d3df6edb2acb18
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-12-05 05:26:31 +0530

    Time: 225 ms (60.00%), Space: 18 MB (8.00%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.py
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.py
@@ -2,10 +2,4 @@ class Solution:
     def minMoves(self, nums: List[int]) -> int:
         minVal = min(nums)
         return sum([x-minVal for x in nums])
-    
-    '''
-    Since you have to increment n-1 elements you will need to increment the
-    minimum element at least (maxE - minE) times and then all other elements will be incremented (x-minE) to catch to make them all equal
-    
-    '''
         
