f43a8dead1644016dbc595e0acae2f96f0f9de0c
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-12-08 11:41:03 -0500

    Time: 4602 ms (23.15%), Space: 16.5 MB (16.22%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -0,0 +1,32 @@
+class Solution:
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        def findNeighbors(r,c):
+            nei = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+            neighbors = []
+            for dR, dC in nei:
+                newR, newC = r+dR, c+dC
+                if 0 <= newR < ROWS and 0 <= newC < COLS and grid[newR][newC] not in [0, '#']:
+                    neighbors.append((newR, newC))
+            neighbors.sort(key = lambda x: grid[x[0]][x[1]])
+            return neighbors
+        def backtrack(r, c):
+            oldVal = grid[r][c]
+            grid[r][c] = '#'
+            neighbors = findNeighbors(r,c)
+            maxNeighbor = 0
+            for nR, nC in neighbors:
+                maxNeighbor = max(maxNeighbor, backtrack(nR, nC))
+            grid[r][c] = oldVal
+            return oldVal + maxNeighbor
+                
+        maxGold = 0
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] > 0:
+                    maxGold = max(maxGold, backtrack(r,c))
+        return maxGold
+                        
+                
+        
+        
