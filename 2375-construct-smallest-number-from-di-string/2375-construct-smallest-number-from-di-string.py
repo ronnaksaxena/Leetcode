193c0a7ec446b06c8322b62e48582f553608af7a
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-12-09 14:25:45 -0500

    Time: 38 ms (66.22%), Space: 16.4 MB (25.47%) - LeetHub

--- 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
@@ -1,20 +1,30 @@
 class Solution:
     def smallestNumber(self, pattern: str) -> str:
-        n = 1
-        res = [str(n)]
-
-        for i, p in enumerate(pattern):
-            print(n, res)
-            if p == "I":
-                res.append(str(n+1))
-                n += 1
-            elif p == "D":
-                ins = i
-                # Finds the right most position to insert n+1 and maintain D
-                while ins >= 0 and pattern[ins] == "D":
-                    ins -= 1
-                res.insert(ins+1, str(n+1))
-                n += 1
+        '''
+        "IIIDIDDD"
+            i
+        curNum = 4
+        num = [1,2,3]
         
-
-        return "".join(res)
+        if p == I:
+            add to end
+        if p == D
+            insertAt = len(num) - 1
+            while num[insertAt]
+        '''
+        n = 1
+        num = ['1']
+        
+        for i, p in enumerate(pattern):
+            if p == 'I':
+                n += 1
+                num.append(str(n))
+            else:
+                ins = i
+                while ins >= 0 and pattern[ins] == 'D':
+                    ins -= 1
+                n += 1
+                # insert at ins + 1 becuase ins is at leftmost I
+                num.insert(ins+1, str(n))
+        return ''.join(num)
+        
