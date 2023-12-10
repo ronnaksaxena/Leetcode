0fba4770fc8f0fee526ee26505fbea11843d7f30
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-12-10 15:54:21 -0500

    Time: 38 ms (79.13%), Space: 16.3 MB (27.52%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.py
@@ -9,53 +9,17 @@ class Solution:
         # Egdge case no tree
         if not root:
             return True
-        def findDepth(root):
-            if not root:
-                return 0
-            return 1 + max(findDepth(root.left), findDepth(root.right))
-        levels = findDepth(root) - 1
-        # Num nodes at level i is 2^i
-        curLevel = 0
         q = collections.deque([root])
+        foundNullNode = False
         while q:
-            if curLevel == levels - 1:
-                # check they are all left leaning
-                numNodes = 2 ** curLevel
-                if len(q) != numNodes:
-                    # Not enough nodes
-                    print('Not enough nodes', node.val)
-                    return False
-                foundRightMost = False
-                # Check for left leaning
-                for i in range(numNodes):
-                    # print(q[i].val, foundRightMost)
-                    if foundRightMost:
-                        if q[i].left or q[i].right:
-                            return False
-                    else:
-                        if  q[i].right and q[i].left:
-                            continue
-                        elif not q[i].left and q[i].right:
-                            return False
-                        else:
-                            foundRightMost = True
-                    
-                # Don't need to check last level
-                break
+            cur = q.popleft()
+            if cur is None:
+                foundNullNode = True
             else:
-                numNodes = 2 ** curLevel
-                if len(q) != numNodes:
-                    # Not enough nodes
-                    print('not enough nodes', [n.val for n in q])
+                if foundNullNode:
                     return False
-                for _ in range(numNodes):
-                    node = q.popleft()
-                    if node.left:
-                        q.append(node.left)
-                    if node.right:
-                        q.append(node.right)
-                curLevel += 1
-            
-            
+                q.append(cur.left)
+                q.append(cur.right)
         return True
+            
         
