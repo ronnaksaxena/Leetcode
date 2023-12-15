86c012f441407adf879a741af505570250ae019a
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-12-15 14:12:31 -0500

    Time: 615 ms (50.56%), Space: 19.3 MB (40.76%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -1,15 +1,16 @@
 class Solution:
     # Changes so that board is ints !!
     def maximalSquare(self, matrix: List[List[int]]) -> int:
-        dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+        maxSide = 0
         for rowIdx, row in enumerate(matrix):
             for colIdx, num in enumerate(row):
+                matrix[rowIdx][colIdx] = int(matrix[rowIdx][colIdx])
                 if (rowIdx == 0 or colIdx == 0) and num == '1':
-                    dp[rowIdx][colIdx] = 1
+                    maxSide = max(maxSide, 1)
                 elif num == '1':
-                    dp[rowIdx][colIdx] = min(dp[rowIdx-1][colIdx], dp[rowIdx][colIdx-1], dp[rowIdx-1][colIdx-1]) + 1
-        ans = max(max(x) for x in dp)
-        return ans **2
+                    matrix[rowIdx][colIdx] = min(matrix[rowIdx-1][colIdx], matrix[rowIdx][colIdx-1], matrix[rowIdx-1][colIdx-1]) + 1
+                    maxSide = max(maxSide, matrix[rowIdx][colIdx])
+        return maxSide ** 2
                 
         
         
