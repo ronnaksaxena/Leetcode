58ef3df2af404c24daa85d53320b33c1138e098f
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-12-21 22:30:58 -0500

    Time: 729 ms (99.12%), Space: 65.7 MB (13.15%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,30 +1,18 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        '''
-        n = 4, k = 2
-        [1,2,3,4]
-    /                   |.      |.     \
-[1]                     [2]     [3].  [4]
-|  \ \  
-[1,2] [1,3] [1,4]      [2, 3] [2,4]
-
-have start i and loop j for all other picks
-if len array you're building == k add to output
-if i > options to pick discard question
-        '''
         
-        output = []
-        
-        def backtrack(cur, i):
-            nonlocal output
-            if i > n+1:
-                return
+        res = []
+        def backtrack(first=1, cur=[]):
             if len(cur) == k:
-                output.append(cur[::])
+                res.append(cur[:])
                 return
-            for j in range(i, n+1):
-                cur.append(j)
-                backtrack(cur, j+1)
+            
+            for i in range(first, n+1):
+                cur.append(i)
+                backtrack(i+1, cur)
                 cur.pop()
-        backtrack([], 1)
-        return output
+            
+            
+            
+        backtrack()
+        return res
