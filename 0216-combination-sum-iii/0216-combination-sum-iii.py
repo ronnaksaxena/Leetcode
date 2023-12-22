02f6d7d3c98c37db5083617ff490f41e746ff396
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2023-12-22 10:17:56 -0500

    Time: 32 ms (91.46%), Space: 17.2 MB (9.34%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,23 +1,31 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        results = []
-        def backtrack(remain, comb, next_start):
-            if remain == 0 and len(comb) == k:
-                # make a copy of current combination
-                # Otherwise the combination would be reverted in other branch of backtracking.
-                results.append(list(comb))
+        '''
+        k = 3, n = 7
+        
+        []
+|   |   |   |   |   |   |
+[1] [2] [3] [4] [5] [6] [7]
+/\
+
+        
+        '''
+        global output
+        output = []
+        def backtrack(cur=[], i=1, remain=n):
+            if len(cur) == k and remain == 0:
+                # Found valid combo
+                output.append(cur[::])
                 return
-            elif remain < 0 or len(comb) == k:
-                # exceed the scope, no need to explore further.
+            if len(cur) > k or remain < 0:
+                # Can't add any more
                 return
-
-            # Iterate through the reduced list of candidates.
-            for i in range(next_start, 10):
-                comb.append(i)
-                backtrack(remain-i, comb, i+1)
-                # backtrack the current choice
-                comb.pop()
-
-        backtrack(n, [], 1)
-
-        return results
+            # All remaining numbers
+            for j in range(i, 10):
+                cur.append(j)
+                backtrack(cur, j+1, remain - j)
+                cur.pop()
+                
+        backtrack()
+        return output
+        
