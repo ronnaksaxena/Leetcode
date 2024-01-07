32b01aee52eeeb640ab7b446bf28ccf0a0dbc97b
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-01-07 13:41:38 -0500

    Time: 97 ms (86.74%), Space: 19.8 MB (40.73%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,31 +1,32 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # Have prevSign as + initially to add 0 to first term
-        curNum, prevSign = 0, '+'
-        stack = []
+        # Stack would have at most 2 parts
+        inner = 0
+        outer = 0
+        ans = 0
+        prevSign = '+'
         s = s.replace(' ', '') # Remove whitestpace
         
         for i in range(len(s)):
             
             if s[i].isnumeric():
-                curNum = curNum*10 + int(s[i])
+                inner = inner*10 + int(s[i])
                 
             # if not elif becuase last char could be num
             if s[i] in '+-/*' or i == len(s)-1:
                 if prevSign == '+':
-                    stack.append(curNum)
+                    # Add last term to result
+                    ans += outer
+                    outer = inner
                 elif prevSign == '-':
-                    stack.append(curNum * -1)
+                    ans += outer
+                    outer = -inner
                 elif prevSign == '*':
-                    # mrc is Most Recent Calculation
-                    mrc = stack.pop()
-                    stack.append(curNum * mrc)
+                    outer *= inner
                 elif prevSign == '/':
-                    mrc = stack.pop()
-                    # int() to handle truncating down negative values
-                    stack.append(int(mrc / curNum))
-                curNum, prevSign = 0, s[i]
+                    outer = int(outer / inner)
+                inner, prevSign = 0, s[i]
                 
-        return sum(stack)
+        return ans + outer
                     
             
