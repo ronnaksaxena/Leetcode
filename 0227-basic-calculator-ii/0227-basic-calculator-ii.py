ef76f570e2896237223f7054a55609bc082c3723
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-01-07 13:37:18 -0500

    Time: 116 ms (67.77%), Space: 20.7 MB (20.99%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,25 +1,29 @@
 class Solution:
     def calculate(self, s: str) -> int:
+        # Have prevSign as + initially to add 0 to first term
         curNum, prevSign = 0, '+'
         stack = []
-        s = s.replace(' ', '')
+        s = s.replace(' ', '') # Remove whitestpace
         
         for i in range(len(s)):
             
             if s[i].isnumeric():
                 curNum = curNum*10 + int(s[i])
                 
+            # if not elif becuase last char could be num
             if s[i] in '+-/*' or i == len(s)-1:
                 if prevSign == '+':
                     stack.append(curNum)
                 elif prevSign == '-':
                     stack.append(curNum * -1)
                 elif prevSign == '*':
+                    # mrc is Most Recent Calculation
                     mrc = stack.pop()
                     stack.append(curNum * mrc)
                 elif prevSign == '/':
                     mrc = stack.pop()
-                    stack.append(int(mrc/ curNum))
+                    # int() to handle truncating down negative values
+                    stack.append(int(mrc / curNum))
                 curNum, prevSign = 0, s[i]
                 
         return sum(stack)
