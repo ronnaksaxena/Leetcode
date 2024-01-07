47b6d45f8d38be5576c2778b06e260e1c2c3c284
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-01-07 17:42:27 -0500

    Time: 73 ms (80.42%), Space: 17.4 MB (29.90%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -5,10 +5,12 @@ class Solution:
         for log in logs:
             pId, op, curTime = log.split(':')
             if op == 'start':
+                # Update elapsed time of currently running program
                 if stack:
                     startTime, program = stack.pop()
-                    # Calculate elapsed time
+                    # Calculate elapsed time, does not include current timeStamp
                     output[program] += (int(curTime) - startTime)
+                    # Next start is at current time?
                     stack.append([int(curTime), program])
                 stack.append([int(curTime), int(pId)])
             else:
@@ -16,6 +18,7 @@ class Solution:
                 startTime, program = stack.pop()
                 output[program] += (int(curTime) - startTime + 1)
                 if stack:
+                    # Previous program in stack starts at next timestamp
                     stack[-1][0] = int(curTime) + 1
         return output
         
