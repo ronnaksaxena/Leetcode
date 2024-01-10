3b63528e3b0ee45d38e47bcb08319eea6ef7f95c
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-01-10 13:41:27 -0500

    Time: 79 ms (53.03%), Space: 17.4 MB (33.62%) - LeetHub

--- 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
+++ 0636-exclusive-time-of-functions/0636-exclusive-time-of-functions.py
@@ -1,22 +1,22 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         output = [0 for _ in range(n)]
-        stack = [] # [startTime, programID]
+        stack = [] # [pId, time]
+        
         for log in logs:
-            pId, op, curTime = log.split(':')
-            if op == 'start':
-                stack.append([int(curTime), int(pId)])
+            pId, operation, timestamp = log.split(':')
+            pId = int(pId)
+            timestamp = int(timestamp)
+            
+            if operation == 'start':
+                stack.append([pId, timestamp])
             else:
-                duration = int(curTime) - stack.pop()[0] + 1
-                output[int(pId)] += duration
+                p, t = stack.pop()
+                duration = timestamp - t + 1
+                output[p] += duration
+                # If program was halted decriment by this program duration
                 if stack:
-                    output[stack[-1][1]] -= duration
+                    output[stack[-1][0]] -= duration
+            
         return output
-        
-        
-        
-        
-        
-        
-        
         
