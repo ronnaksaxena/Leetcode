a1e367ee0b426cfbdc32b522f950ca78a3300657
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-01-25 19:21:26 -0500

    Time: 144 ms (86.63%), Space: 19.8 MB (92.32%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,19 +1,47 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        diagonals = collections.defaultdict(list)
-        ROWS, COLS = len(mat), len(mat[0])
-        for r in range(ROWS):
-            for c in range(COLS):
-                # All elements that share a sum of r and c are in same diagonal
+        '''
+        - rectangle matrix
+        - can be duplicate values
+        - flip order of each diagonal
+        
+        
+        [1,2,3, 10]
+        [4,5,6, 11]
+        [7,8,9, 12]
+        
+        0 + 0 => 0 : [1]
+        0 + 1, 1 + 0 => 1 [2, 4]
+        2 + 0, 1 + 1, 0 + 2 => 2 [3, 5, 7]
+        2 + 1, 2 + 1 => 3 [6,8]
+        2 + 2 => 4 [9]
+        
+        diagonal idx == r + c
+        idea: hashmap {diagonal index: [elements in that diagonal]}
+        
+        if diagonal index is even: reverse order of elements that were added
+        
+        - Traverse matrix
+            - add element to hashmap based on diagonal index
+        - Traverse map by diagonal index
+            - if diagonal index is even: reverse order of elements in that map
+        Time: O(m x n) where m is rows and n is colums
+        Space: O(m x n) to store all values in map
+        
+        '''
+        diagonals = collections.defaultdict(list) # {diagonal index: [elements]}
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
                 diagonals[r+c].append(mat[r][c])
         output = []
-        # Build diagonal traversal
-        for d in sorted(diagonals.keys()):
-            # If even diagonal reverse elements order
-            if d % 2 == 0:
-                output.extend(diagonals[d][::-1])
+        for dIndex in sorted(diagonals.keys()):
+            if dIndex % 2:
+                output.extend(diagonals[dIndex])
             else:
-                output.extend(diagonals[d])
-        return output
+                # Reverse even diagonals
                 
+                output.extend(diagonals[dIndex][::-1])
+        return output
+        
+        
         
