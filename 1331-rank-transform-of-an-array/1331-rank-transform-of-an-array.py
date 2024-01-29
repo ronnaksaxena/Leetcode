00e846ef869b051ee9dd4a604260e04adabe0adc
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-01-29 16:58:11 -0500

    Time: 268 ms (49.43%), Space: 33.3 MB (91.40%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -1,21 +1,8 @@
 class Solution:
     def arrayRankTransform(self, arr: List[int]) -> List[int]:
-        sortedArr = sorted(arr)
-        ranks = {} # Value: Rank
-        rank = 1
-        i = 0
-        while i < len(sortedArr):
-            ranks[sortedArr[i]] = rank
-            rank += 1
-            i += 1
-            while i < len(sortedArr) and sortedArr[i] == sortedArr[i-1]:
-                i += 1
-        
-        output = [0 for _ in range(len(arr))]
-        for j in range(len(arr)):
-            output[j] = ranks[arr[j]]
-        
-        return output
-        
+        rank = {}
+        for n in sorted(arr):
+            rank[n] = rank.get(n, len(rank) + 1)
+        return [rank[n] for n in arr]
             
         
