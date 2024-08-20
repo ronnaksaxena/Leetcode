833ef5ade4f9c478eba0a678ef8e1b3897c084f6
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-08-19 22:08:28 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.py
@@ -8,38 +8,26 @@ class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         '''
         -10
-      /.   \
-     9.    20
-          /. \
-         15.  7
-         
-    max path is either:
-    1. just current node
-    2. left
-    3. right
-    4. node + left + right
-    
-    bottom up solution 
-    Time: O(n)
-    Space: O(1)
-        
+       /. \     
+    -100.  -10
+
         '''
-        maxPath = float('-inf')
+        # EDGE CASE
+        if not root:
+            return 0
+
+        maxSum = float(-inf)
         def dfs(node):
-            nonlocal maxPath
+            nonlocal maxSum
             if not node:
                 return 0
+            
             left = dfs(node.left)
             right = dfs(node.right)
-            maxPath = max(maxPath, node.val, node.val + left, node.val + right, node.val + left + right)
-            return max(node.val + left, node.val + right, node.val)
-            
+            maxSum = max(maxSum, node.val + left, node.val + right, node.val + left + right, node.val)
+            return node.val + max(left, right)
+
         dfs(root)
-        return maxPath
+        return maxSum
             
-            
-            
-            
-            
-            
-            
+        
