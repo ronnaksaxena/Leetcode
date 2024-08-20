9ac3ec0ed31846de9c1042c8b8acbd6284a251c9
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-08-19 21:58:02 -0700

    Time: 33 ms (79.46%), Space: 17 MB (96.34%) - LeetHub

--- 0426-convert-binary-search-tree-to-sorted-doubly-linked-list/0426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
+++ 0426-convert-binary-search-tree-to-sorted-doubly-linked-list/0426-convert-binary-search-tree-to-sorted-doubly-linked-list.py
@@ -9,42 +9,27 @@ class Node:
 
 class Solution:
     def treeToDoublyList(self, root: 'Optional[Node]') -> 'Optional[Node]':
-        '''
-            4
-        /.      \
-        2       5
-    /       \
-1           3
-        
-        h 1
-        t 1
-        
-        '''
+        # EDGE CASE
         if not root:
-            return root
+            return None
         head, tail = None, None
-        
-        def dfs(root):
-            nonlocal head
-            nonlocal tail
-            stack = []
-            cur = root
-            while stack or cur:
-                while cur:
-                    stack.append(cur)
-                    cur = cur.left
-                cur = stack.pop()
-                if not head:
-                    head = cur
-                if tail:
-                    tail.right = cur
-                cur.left = tail
-                tail = cur
-                cur = cur.right
+        def dfs(node):
+            nonlocal head, tail
+            if not node:
+                return
             
+            dfs(node.left)
+            if not head:
+                head = node
+                tail = node
+            else:
+                tail.right, node.left = node, tail
+                tail = node
+
+            dfs(node.right)
+
         dfs(root)
-        # Connect tail and head
-        head.left, tail.right = tail, head
+        # Complete cycle
+        tail.right, head.left = head, tail
         return head
-            
         
