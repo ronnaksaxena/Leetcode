f38ae1972e035f2e0649e76b74813ebc7d0b0b44
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-08-23 20:03:55 -0700

    Time: 96 ms (10.52%), Space: 18.8 MB (8.27%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,32 +1,59 @@
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        
-        par = [i for i in range(n)]
-        rank = [1 for _ in range(n)]
-        
-        def find(n):
-            while n != par[n]:
-                par[n] = par[par[n]]
-                n = par[n]
-            return n
-        def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
-            if p1 == p2:
+        '''
+        input: n:int, edges: List of List int
+        output: bool
+
+        -len(edges) == n
+        -n can be 0
+        -node cannot point to itself
+
+        parent -> child relationshp which means NO CYCLES
+
+        Algo: Traverse graph and any neighbors (EXCLUDING PARENT) have been visited there is a cycle
+
+                4
+             /    \. \
+             1.   2.   3
+             /
+             0
+
+        helper fn isParent(child, parent):
+            return child in edges[parent]
+
+        if any neighbors have been visted and are not parent of node return False
+
+        PSUEDOCODE
+
+        stack = [0]
+        visited = set{0}
+
+        while stack:
+            cur = stack.pop()
+            visited.add(cur)
+            for neighbor in edges[cur]:
+                if neighbor in visited and not isParent(cur, neighbor):
+                    return False
+                stack.append(neighbor)
+
+
+        return len(visited) == n
+        '''
+        graph = collections.defaultdict(list)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
+        def dfs(node, parent, visited):
+            if node in visited:
                 return False
-            else:
-                if rank[p1] > rank[p2]:
-                    par[p2] = p1
-                elif rank[p1] < rank[p2]:
-                    par[p1] = p2
-                else:
-                    par[p1] = p2
-                    rank[p2] += 1
-                return True
+            visited.add(node)
+            for nei in graph[node]:
+                if nei != parent:
+                    if not dfs(nei, node, visited):
+                        return False
+            return True
+        visited = set()
+        return dfs(0, -1, visited) and len(visited) == n
+
         
-        # Check no redudant connections in edges
-        for v1, v2 in edges:
-            if not union(v1, v2):
-                return False
-        # Check all have same parent
-        # Par array may not be updated yet so need to call find
-        return all(find(x) == find(0) for x in range(n))
