79ef20b3229c6debf39c5c4a1d690dcc12ae5d34
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-08-31 22:03:57 -0700

    Time: 37 ms (67.04%), Space: 16.5 MB (74.94%) - LeetHub

--- 0269-alien-dictionary/0269-alien-dictionary.py
+++ 0269-alien-dictionary/0269-alien-dictionary.py
@@ -1,58 +1,43 @@
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
+        in_degree = {}
+        graph = {}
 
-        '''
-        w - > e
-        t -> f
+        for w in words:
+            for c in w:
+                in_degree[c] = 0
+                graph[c] = []
 
-        1. iterate thorugh words and construct graph
-            - aab, aa
-        2. find top ordering of letter
-        3. check we were able to traverse every letter
-
-        Time: O(E+V) or O(number of words - 1 + letters in words)
-        Space: O(V) or O(letters)
-        '''
-
-        graph = collections.defaultdict(set)
-        inDegree = collections.defaultdict(int)
-        for word in words:
-            for c in word:
-                if c not in graph:
-                    graph[c] = set()
-                if c not in inDegree:
-                    inDegree[c] = 0
-        # Looking for relationship of letters form words
+        numLetters = len(in_degree)
+        
         for first, second in zip(words, words[1:]):
-            # Handle edge case ex. aab, aa
-            foundEdge = False
-            for f, s in zip(first, second):
-                if f != s:
-                    foundEdge = True
-                    # Only want to add letter relationship once
-                    if s not in graph[f]:
-                        graph[f].add(s)
-                        inDegree[s] += 1
+            # Find first letter that differs
+            difLetter = None
+            for a, b in zip(first, second):
+                if a != b:
+                    difLetter = a
+                    in_degree[b] += 1
+                    graph[a].append(b)
                     break
-            if not foundEdge and len(second) < len(first):
-                # No ordering exists
+            if len(second) < len(first) and not difLetter:
+                # No order possible
                 return ''
-        order = []
+        
         q = collections.deque()
-        for c in inDegree:
-            if inDegree[c] == 0:
-                q.append(c)
+        for letter, deg in in_degree.items():
+            if not deg:
+                q.append(letter)
+        
+        order = []
         while q:
-            letter = q.popleft()
-            order.append(letter)
-            for nextLetter in graph[letter]:
-                inDegree[nextLetter] -= 1
-                if inDegree[nextLetter] == 0:
+            cur = q.popleft()
+            order.append(cur)
+
+            for nextLetter in graph[cur]:
+                in_degree[nextLetter] -= 1
+                if not in_degree[nextLetter]:
                     q.append(nextLetter)
-        # If you could traverse all letters, ordering exists
-        return ''.join(order) if len(order) == len(graph.keys()) else ''
+        
+        return ''.join(order) if len(order) == numLetters else ''
 
         
-
-
-
