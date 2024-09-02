80339d2086afe71a33efd44d61bccb68e1accae4
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-09-01 20:32:31 -0700

    Time: 53 ms (55.36%), Space: 17.3 MB (11.25%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,29 +1,41 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         '''
-        [2,3,5], target = 8
-        output = [[2, 2, 2, 2], [2, 2, 3], [3,5]]
-        
-        combo = [2, 2, 3], curSum = 0
-        terminal: if curSum == target: add copy to output
-                  if curSum > target: return
-        
+    candidates = [2,3,6,7], target = 7
+
+i              [2, 2, 3]
+
+Backtrack:
+    Terminals
+    - if sum == target: add to output
+    - if sum > target: return
+
+    - pick element
+    - dont pick element
         '''
-        
+
         output = []
         
-        def backtrack(combo = [], curSum = 0, start = 0):
+        def backtrack(i=0, combo=[], curSum = 0):
             nonlocal output
             if curSum == target:
-                output.append(copy.deepcopy(combo))
+                output.append(combo[:])
                 return
             if curSum > target:
                 return
-            for i in range(start, len(candidates)):
-                combo.append(candidates[i])
-                backtrack(combo, curSum + candidates[i], i)
-                combo.pop()
-        
+            if i == len(candidates):
+                return
+            # Pick
+            combo.append(candidates[i])
+            backtrack(i, combo, curSum + candidates[i])
+            # Don't pick
+            combo.pop()
+            backtrack(i+1, combo, curSum)
+
         backtrack()
-        return list(output)
+        return output
+
+        
+
+
         
