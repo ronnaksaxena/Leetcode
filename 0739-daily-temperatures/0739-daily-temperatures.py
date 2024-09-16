59b597f08f06b1e58c01f246e1664c356e857805
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-09-16 12:42:49 -0700

    Time: 866 ms (84.88%), Space: 30.6 MB (89.66%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,12 +1,40 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n = len(temperatures)
-        output = [0 for _ in range(n)]
-        stack = []
+        '''
+        input: int[]
+        output: int[]
+
+        Can be empty? => No
+        Valid temps? => Yes
+        If no future days => ans[i] = 0
+
+        [73,74,75,71,69,72,76,73]
+                              i
+
+        stack = [6, 7]
+        [1,1,4,2,1,1,0,0]
+
+        [big -> small -> smaller] Biggest is next largest day for all elements
+        To get next largest element need to maintain monotomically decreasing stack
+        while top of stack temp is < current temp, 
+            prevDay = stack.pop()
+            ans[prevDay] = i - prevDay ==> deltas of days
+        stack.append(i)
+
+        Time: O(n)
+        Spacre: O(n)
         
+        '''
+        stack = []
+        n = len(temperatures)
+        ans = [0] * n
+
         for i, t in enumerate(temperatures):
             while stack and temperatures[stack[-1]] < t:
-                wait = i - stack[-1]
-                output[stack.pop()] = wait
+                prevDay = stack.pop()
+                ans[prevDay] = i - prevDay
+            
             stack.append(i)
-        return output
+
+        return ans
+        
