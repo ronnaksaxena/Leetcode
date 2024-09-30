4b57782d9ef00e2ce33ac3ccb7563a48ad40d943
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-09-29 21:24:24 -0700

    Time: 107 ms (59.85%), Space: 16.8 MB (66.82%) - LeetHub

---
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+        '''
+        stations is [location, fuel]
+        append target to stations to see if you can drive by it
+
+        Intution can drive by stations until you run of gas
+
+        Algo:
+        Store stations with most fuel in max heap
+        Calculate how much gas you have left at a certain spot
+            -if out of gas (fuel < 0) can retroactively fill fuel by looking at past large gas stastions
+            -if heap is empty and fule is still < 0 there is no solution
+
+
+        '''
+        stations.append([target, float('inf')])
+        prev, curFuel = 0, startFuel
+        maxHeap = []
+        stops = 0
+
+        for location, fuel in stations:
+            distance = location - prev
+            curFuel -= distance
+            while maxHeap and curFuel < 0:
+                curFuel += -heapq.heappop(maxHeap)
+                stops += 1
+            # No solution :\ ran out of gas stations
+            if curFuel < 0:
+                return -1
+            # Add this station to our pq to come back to later if needed
+            prev = location
+            heapq.heappush(maxHeap, -fuel)
+
+        return stops
+
+        
