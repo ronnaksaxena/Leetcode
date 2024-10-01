3f037b2a80aa7b7e769cdab5d7144c15166c591f
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-09-30 17:23:54 -0700

    Time: 147 ms (83.79%), Space: 17.8 MB (96.44%) - LeetHub

--- 1231-divide-chocolate/1231-divide-chocolate.py
+++ 1231-divide-chocolate/1231-divide-chocolate.py
@@ -1,45 +1,24 @@
 class Solution:
     def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
-        '''
-        -condition(x) is x a workable value?
-            - can cut into k+1 pieace and each piece sweetness >= x
-
-        - search space
-            left is min(sweetness) can't have a chunk < min piece
-            right is sum(sweetness) // people or k + 1
-                if everyone gets a piece this is largest I can get myself
-        
-        return: left
-
-        '''
-
-        def isSweetEnough(sweetLevel):
-            peopleWithChocolate = 0
-            curSweet = 0
+        def canFeedEveryone(sweetLevel):
+            peopleFed = 0
+            currentSweetness = 0
             for s in sweetness:
-                curSweet += s
-                if curSweet >= sweetLevel:
-                    peopleWithChocolate += 1
-                    curSweet = 0
-            return peopleWithChocolate >= k + 1
+                currentSweetness += s
+                if currentSweetness >= sweetLevel:
+                    peopleFed += 1
+                    currentSweetness = 0
+            return peopleFed >= (k+1)
 
-        left = min(sweetness)
-        right = sum(sweetness) // (k + 1)
+        l = min(sweetness)
+        r = sum(sweetness) // (k+1)
 
-        while left < right:
-            '''
-            Why + 1?
-            T T T F F
-                l r
-                m will be where l is and eval to True
-            new boundary will be [l, r] and be inf loop
-
-            '''
-            mid = 1 + left + (right - left) // 2
-            if isSweetEnough(mid):
-                left = mid
+        while l < r:
+            m = 1 + l + (r-l) // 2
+            print(l, r, m)
+            if canFeedEveryone(m):
+                l = m
             else:
-                right = mid - 1
-        
-        return left
+                r = m - 1
+        return l
         
