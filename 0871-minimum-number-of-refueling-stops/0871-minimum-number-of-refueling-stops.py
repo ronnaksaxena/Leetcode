a4117bb52662f0bc416eec500f15f7d8fcd0399d
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-09-30 17:07:16 -0700

    Time: 98 ms (93.31%), Space: 16.9 MB (33.13%) - LeetHub

--- 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.py
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.py
@@ -1,38 +1,26 @@
 class Solution:
     def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
-        '''
-        stations is [location, fuel]
-        append target to stations to see if you can drive by it
 
-        Intution can drive by stations until you run of gas
-
-        Algo:
-        Store stations with most fuel in max heap
-        Calculate how much gas you have left at a certain spot
-            -if out of gas (fuel < 0) can retroactively fill fuel by looking at past large gas stastions
-            -if heap is empty and fule is still < 0 there is no solution
-
-        Time: O(NlogN) to iterate through stastions & heap operations
-        Space: O(N) in worst case use heap
-        '''
-        stations.append([target, float('inf')])
-        prev, curFuel = 0, startFuel
-        maxHeap = []
         stops = 0
+        maxHeap = []
+        stations.append([target, 0])
+        tank = startFuel
+        prevLocation = 0
 
         for location, fuel in stations:
-            distance = location - prev
-            curFuel -= distance
-            while maxHeap and curFuel < 0:
-                curFuel += -heapq.heappop(maxHeap)
+            tank -= (location-prevLocation)
+            while tank < 0 and maxHeap:
+                tank += -heapq.heappop(maxHeap)
                 stops += 1
-            # No solution :\ ran out of gas stations
-            if curFuel < 0:
+            if tank < 0:
                 return -1
-            # Add this station to our pq to come back to later if needed
-            prev = location
+            if location == target:
+                return stops
+            prevLocation = location
             heapq.heappush(maxHeap, -fuel)
 
         return stops
+
+
 
         
