2d925d4ffda94464a6946ab7ea6a5aa574b5f91b
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-10-01 22:01:47 -0700

    Time: 121 ms (15.8%), Space: 17 MB (88.76%) - LeetHub

--- 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
+++ 0849-maximize-distance-to-closest-person/0849-maximize-distance-to-closest-person.py
@@ -1,40 +1,15 @@
-class Solution:
-    def maxDistToClosest(self, seats: List[int]) -> int:
-        '''
-        [1,0,0,0,1,0,1]
-    l = [0,1,2,3,0,1,0]
-    r = [0,3,2,1,0,1,0]
+class Solution(object):
+    def maxDistToClosest(self, seats):
+        N = len(seats)
+        left, right = [N] * N, [N] * N
 
-    ans = max(ans, min(left[i], right[i]))
+        for i in range(N):
+            if seats[i] == 1: left[i] = 0
+            elif i > 0: left[i] = left[i-1] + 1
 
-        '''
+        for i in range(N-1, -1, -1):
+            if seats[i] == 1: right[i] = 0
+            elif i < N-1: right[i] = right[i+1] + 1
 
-        n = len(seats)
-        left = [0] * n
-        right = [0] * n
-
-        # Forward pass to init left\
-        for i in range(1, n):
-            if seats[i] == 0:
-                left[i] = left[i-1] + 1
-
-        # Backward pass to init right
-        for i in range(n-2, -1, -1):
-            if seats[i] == 0:
-                right[i] = right[i+1] + 1
-
-
-        # Forward pass to calc answer
-        # print(left, right)
-        ans = 0
-        for i, (l, r) in enumerate(zip(left, right)):
-            if i == 0:
-                ans = max(ans, r)
-            elif i == n-1:
-                ans = max(ans, l)
-            else:
-                ans = max(ans, min(l, r))
-
-        return ans
-
-        
+        return max(min(left[i], right[i])
+                   for i, seat in enumerate(seats) if not seat)
