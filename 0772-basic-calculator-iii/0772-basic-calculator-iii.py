dccad000a1a60ee779b585336683c94e15109802
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-10-03 14:08:58 -0700

    Time: 38 ms (77.5%), Space: 16.7 MB (66.64%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.py
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.py
@@ -1,84 +1,86 @@
 class Solution:
     def calculate(self, s: str) -> int:
         '''
-        evalute when you reach an operator or )
-        input: string, output: int
+        Qs:
+        - input: string, output: int (evaluation)
+        - valid s, nonempty
+        - '+', '-', '*', '/' , (),
+            - / to truncate towards 0 int(dividend/ divisor)
+            - no divide by 0
+        - valid answer to each expression
 
-        - truncate down on /
-        - valid input, numeric with operators
+        Idea: store like terms in a stack that can be summed to create final answer
+
+        2 + 2 * 5 <- +
+                       
+        s = [2, 10] sum(stack) => 12
+        prevOperation = *
+        push onto stack whenever we encounter an operation
 
 
-        - evaluate in order of operations, (), */, +-
-        - stack to store terms that sum up to answer
+        2*(5+5*2)/3+(6/2+8) <- +
+                    i
+        s = [10]
+        set cur to evaluation of term
+        prevOperand = stack.pop() => *
 
-        stack = [1, answer]
+        When reach ) evaluate all terms up until operand and then evaluate parenthesis with initial operate retrieved from the stack
 
-        - if encoutner opening parenthesis push operator p
-        - ((1+1))
-        [2]
-        - if encounter ) while top of stack != p:
-            pop and aggregate a sum of that term in the parenthesis
+        Algo:
+            -def helperEvaluate(operation, arg, arg2)
+            - init stack, cur int, prevOperand string
+            - iterate through the input and push terms onto stack
+            - return sum(stack)
 
-        s = "2*(5+5*2)/3+(6/2+8)" <- '+'
-                     i
-        stack = [2, 'p', 5, 10]
-        cur = 2
-        prevOperator = *
+        Time: O(N * number of parethesis pairs)
+        Space: O(N) for stack
 
-    1. digit update cur
-    2. opening parenthesis, push p onto stack, evaluate current term + push onto stack
-        prevOperator = +
-    3. # closing parenthese or new operatore
-        evalue previous term + push onto stack
-        while top of stack != p
-            temp += stack.top
 
-        push temp onto stack
-    4. sum all terms i stack
-
-    Time: O(N) for our stack
-    Space: O(N) for stack
         '''
 
-        def evaluate(operand, first, second=0):
-            if operand == '+':
-                return first
-            if operand == '-':
-                return -first
-            if operand == '*':
-                return first*second
+        def evaluate(operation, x, y=0):
+            if operation == '+':
+                return x
+            elif operation == '-':
+                return -x
+            elif operation == '*':
+                return x*y
             else:
-                # integer division
-                return int(first/second)
+                # truncate to 0
+                return int(x/y)
         
-        cur, stack, prevOperand = 0, [], '+'
+        stack = []
+        curNumber = 0
+        prevOperand = '+'
+
         s += '+'
 
         for c in s:
-            if c.isnumeric():
-                cur = cur*10 + int(c)
-            elif c == '(': # FIXME
+            # print(c, stack, curNumber, prevOperand)
+            if c.isdigit():
+                curNumber = (curNumber*10) + int(c)
+            elif c == '(':
                 stack.append(prevOperand)
                 prevOperand = '+'
             else:
-                # closing parenthesis or new operand
+                # operation
                 if prevOperand in '+-':
-                    stack.append(evaluate(prevOperand, cur))
+                    stack.append(evaluate(prevOperand, curNumber))
+                    curNumber = 0
+                    prevOperand = c
+                # closing parenthesis
                 elif prevOperand in '*/':
-                    first = stack.pop()
-                    stack.append(evaluate(prevOperand, first, cur))
-                cur = 0
-                prevOperand = c
-                # Is this a closing parenthesis?
+                    firstArg = stack.pop()
+                    stack.append(evaluate(prevOperand, firstArg, curNumber))
+                    curNumber = 0
+                    prevOperand = c
                 if c == ')':
-                    tempTerm = 0
-                    while type(stack[-1]) == int:
-                        tempTerm += stack.pop()
-                    cur = tempTerm
+                    while stack and type(stack[-1]) == int:
+                        curNumber += stack.pop()
+                    
                     prevOperand = stack.pop()
 
         return sum(stack)
-
 
 
         
