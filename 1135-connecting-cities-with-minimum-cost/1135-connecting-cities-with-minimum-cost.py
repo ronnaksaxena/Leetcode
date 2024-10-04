b6bae0a332492c42aed78f5335793be440439d25
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-10-04 13:07:49 -0700

    Time: 459 ms (99.29%), Space: 22.6 MB (63.55%) - LeetHub

--- 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
+++ 1135-connecting-cities-with-minimum-cost/1135-connecting-cities-with-minimum-cost.py
@@ -1,51 +1,38 @@
-class UF:
-    def __init__(self, n):
-        self.par = {i: i for i in range(1, n+1)}
-        self.rank = {i: 1 for i in range(1, n+1)}
-
-    def find(self, x):
-        while x != self.par[x]:
-            self.par[x] = self.par[self.par[x]]
-            x = self.par[x]
-        return x
-
-    def isConnected(self, n1, n2):
-        return self.find(n1) == self.find(n2)
-    
-    def union(self, n1, n2):
-        p1, p2 = self.find(n1), self.find(n2)
-        if self.rank[p1] > self.rank[p2]:
-            self.par[p2] = p1
-        elif self.rank[p2] > self.rank[p1]:
-            self.par[p1] = p2
-        else:
-            self.par[p2] = p1
-            self.rank[p1] += 1
-    
-
-class Edge:
-    def __init__(self, v1, v2, cost):
-        self.v1 = v1
-        self.v2 = v2
-        self.cost = cost
-    
-    def __lt__(self, other):
-        return isinstance(other, Edge) and self.cost < other.cost
-class Solution:
-    def minimumCost(self, n: int, connections: List[List[int]]) -> int:
-        minHeap = []
-        for v1, v2, cost in connections:
-            heapq.heappush(minHeap, Edge(v1, v2, cost))
-
-        visited = set()
-        uf = UF(n)
-        totalCost = 0
-        while minHeap:
-            e = heapq.heappop(minHeap)
-            if not uf.isConnected(e.v1, e.v2):
-                uf.union(e.v1, e.v2)
-                totalCost += e.cost
-
-        return totalCost if all(uf.isConnected(1, x) for x in range(1, n+1)) else -1
-
+class Solution(object):
+    def minimumCost(self, n, connections):
+        """
+        :type n: int
+        :type connections: List[List[int]]
+        :rtype: int
+        """
+        parent = [i for i in range (n+1)]
+        rank = [0] *(n+1)
         
+        def find(n1):
+            p1 = parent[n1]
+            while p1 != parent[p1]:
+                parent[p1] = parent[parent[p1]]
+                p1 = parent[p1]
+            return p1
+        def union(n1,n2):
+            p1,p2 = find(n1),find(n2)
+            if p1 == p2 :
+                return False
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+        connections = sorted(connections,key= lambda x:x[2])
+        output = 0
+        no_of_disconnected_graphs = n
+        for edge in connections:
+            if union(edge[0],edge[1]) == True:
+                output += edge[2]
+                no_of_disconnected_graphs -= 1
+                if no_of_disconnected_graphs == 1:
+                    return output
+        return -1
+     
