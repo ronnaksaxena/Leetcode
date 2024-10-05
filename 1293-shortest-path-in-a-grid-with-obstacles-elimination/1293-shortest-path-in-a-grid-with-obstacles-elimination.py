a63831d5f143ade9e339bce442fe574cb21390d2
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-10-04 21:06:30 -0700

    Time: 128 ms (70.78%), Space: 17 MB (90.53%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,42 +1,33 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        rows, cols = len(grid), len(grid[0])
-        # Cache best path found
-        mostLives = [[float('-inf') for _ in range(cols)] for _ in range(rows)]
+        m, n = len(grid), len(grid[0])
+        target = (m-1, n-1)
+        best = {} # (r,c): lives
+
+        q = collections.deque([(0, 0, k)]) # row, col, removals left
+
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         
-        target = (rows-1, cols-1)
-        visited = {(0,0, k)}
-        q = collections.deque([(0,0, k)]) #(row, col, livesLeft)
-        dirs = [[-1, 0], [1, 0], [0, -1], [0,1]]
-        steps = 0
+        depth = 0
         while q:
-            
             for _ in range(len(q)):
-                curRow, curCol, curLives = q.popleft()
-                
-                if (curRow, curCol) == target:
-                    return steps
-                # Have ot remove obstacles
-                if grid[curRow][curCol] == 1:
-                    curLives -= 1
-                # Out of lives :(
-                if curLives < 0:
-                    continue
-                if mostLives[curRow][curCol] > curLives:
-                    continue # This path sucks
-                else:
-                    mostLives[curRow][curCol] = curLives
-                
-                for dR, dC in dirs:
-                    newR, newC = curRow+dR, curCol+dC
-                    if (newR, newC, curLives) not in visited and 0 <= newR < rows and 0 <= newC < cols:
-                        q.append((newR, newC, curLives))
-                        visited.add((newR, newC, curLives))
-                        
-            steps += 1
-        # no possible path
-        return -1
-                
-        
-        
+                curR, curC, livesLeft = q.popleft()
+
+                if (curR, curC) == target:
+                    return depth
+
+                if grid[curR][curC] == 1:
+                    livesLeft -= 1
+
+                if livesLeft >= 0:
+                    for dR, dC in dirs:
+                        newR, newC = curR + dR, curC + dC
+                        if 0 <= newR < m and 0 <= newC < n and livesLeft > best.get((newR,newC), float('-inf')):
+                            best[(newR, newC)] = livesLeft
+                            q.append((newR, newC, livesLeft))
+
+            depth += 1
+
+        return -1 # no path found
+
         
