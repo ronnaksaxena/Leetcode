1bc3fa89c0d55970636ac0940ac540b9cf83c0d9
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-10-17 21:43:18 -0700

    Time: 110 ms (100%), Space: 39.6 MB (6.94%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,63 +1,32 @@
-import heapq
 class MedianFinder:
-    '''
-    maxH
-    -1
-    1
-    2
-    
-    
-    minH
-    3
-    5
-    addNum algo:
-    1. Add to respective heap if val <= maxH[-1]: push -> maxH
-        else push -> minH
-    2. maintain order of heap if abs(len(maxH) - len(minH)) >= 2
-    if maxH length > minH length: pop from maxH and push to minH
-    else pop from minH and push to minH
-    
-    worst case time O(logN), space O(N)
-    
-    finMedian algo:
-    if len(maxH) == len(minH):
-        return (maxH[-1] + minH[-1]) / 2
-    else:
-        return top of whichever heap is longer
-    time: O(1)
-    '''
 
     def __init__(self):
-        self.minH = []
-        self.maxH = []
+        self.minHeap = []
+        self.maxHeap = []
+        
 
     def addNum(self, num: int) -> None:
-        # CAREFUL of negative values for max heap
-        # Add to respective heap
-        if self.maxH and num <= -self.maxH[0]:
-            heapq.heappush(self.maxH, -num) # negative to maintain max heap
+        if self.minHeap and num >= self.minHeap[0]:
+            heapq.heappush(self.minHeap, num)
         else:
-            heapq.heappush(self.minH, num)
-        # Maintain heaps structure
-        if abs(len(self.minH) - len(self.maxH)) >= 2:
-            # Max Heap is longer
-            if len(self.maxH) > len(self.minH):
-                heapq.heappush(self.minH, -heapq.heappop(self.maxH))
-            # Min Heap is longer
+            heapq.heappush(self.maxHeap, -num)
+        
+        if abs(len(self.minHeap) - len(self.maxHeap)) > 1:
+            if len(self.minHeap) > len(self.maxHeap):
+                element = heapq.heappop(self.minHeap)
+                heapq.heappush(self.maxHeap, -element)
             else:
-                heapq.heappush(self.maxH, -heapq.heappop(self.minH))
-        return
-            
+                element = -heapq.heappop(self.maxHeap)
+                heapq.heappush(self.minHeap, element)
+
         
 
     def findMedian(self) -> float:
-        # CAREFUL of negative values for max heap
-        # Even number of values so take median
-        if len(self.minH) == len(self.maxH):
-            return (self.minH[0] + (-1 *self.maxH[0])) / 2
-        # Odd number of values so take middle element
-        else:
-            return self.minH[0] if len(self.minH) > len(self.maxH) else (-1 *self.maxH[0])
+        # print(f'minHeap {self.minHeap} maxHeap {self.maxHeap}')
+        if len(self.minHeap) == len(self.maxHeap):
+            return ((-self.maxHeap[0] + self.minHeap[0]) / 2)
+        
+        return self.minHeap[0] if len(self.minHeap) > len(self.maxHeap) else -self.maxHeap[0]
         
 
 
