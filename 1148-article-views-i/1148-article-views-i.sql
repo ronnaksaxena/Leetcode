d39050a2fcd6d03ffaad64962c4dee14a99e3407
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-10-23 22:15:44 -0700

    Time: 497 ms (20.36%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-SELECT DISTINCT author_id as 'id' from Views
+SELECT DISTINCT author_id as id
+FROM Views
 WHERE author_id = viewer_id
-ORDER BY author_id
+ORDER BY author_id ASC
