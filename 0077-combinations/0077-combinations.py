7b55c69da0c051f881b6c8d42d0533ad6a8e48eb
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-10-30 10:09:54 -0700

    Time: 143 ms (37.51%), Space: 59.1 MB (84.4%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,18 +1,33 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        
-        res = []
-        def backtrack(first=1, cur=[]):
+        '''
+        - combinations include [1,n] of size k
+
+        n = 4, k = 2
+        1, 2, 3, 4
+        [].  -> loop throught remaining potential candidates
+    /.   /.   \.  - i+1 -> (n-k)
+    [1].    [2]  [3]
+  /. \. \.      / \.  |
+[1,2] [1,3] [1,4] [2,3] [2,4] [3,4] => terminate if size of combnation == k
+
+Time: O(n^k)
+Space: O(k)
+        '''
+
+        output = []
+
+        def backtrack(cur=[], leftBound=1):
+            nonlocal output
             if len(cur) == k:
-                res.append(cur[:])
+                output.append(cur[:])
                 return
-            
-            for i in range(first, n+1):
-                cur.append(i)
-                backtrack(i+1, cur)
+            start = cur[-1]+1 if cur else 1
+            for candidate in range(start, n+1):
+                cur.append(candidate)
+                backtrack(cur)
                 cur.pop()
-            
-            
-            
+
         backtrack()
-        return res
+        return output
+        
