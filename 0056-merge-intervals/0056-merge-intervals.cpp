d8ec186725c5d485f9c30a0a1eedfe213e116ecb
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-10-31 10:00:15 -0700

    Time: 7 ms (44.42%), Space: 23 MB (41.33%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -8,15 +8,13 @@ public:
         cout << endl;
     }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [] (const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
+        sort(intervals.begin(), intervals.end());
         vector<vector<int>> output;
         for (const vector<int>& i: intervals) {
-            if (!output.empty() && output[output.size()-1][1] >= i[0]) {
+            if (!output.empty() && output.back()[1] >= i[0]) {
                 // cout << output[output.size() -1][1] << endl;
                 // cout << max(output[output.size() -1][1], i[1]) << endl;
-                output[output.size() -1][1] = max(output[output.size() -1][1], i[1]);
+                output.back()[1] = max(output.back()[1], i[1]);
                 // printList(output);
             } else {
                 output.push_back(i);
