1211c2a442ebe965d34c6d5893f56ee9e5608559
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-10-31 11:15:42 -0700

    Time: 0 ms (100%), Space: 17.1 MB (21.25%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,20 +1,14 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        '''
-        Time: O(logn)
-        Space: O(1)
-        
-        '''
-        # Needs to be len(nums) -1 because you're comparing to s[r]
+
         l, r = 0, len(nums) - 1
+
         while l < r:
             m = l + (r-l)//2
-            # Can't be pivot since this would mean its ascending
             if nums[m] > nums[r]:
                 l = m + 1
             else:
                 r = m
-        
+
         return nums[l]
-                
         
