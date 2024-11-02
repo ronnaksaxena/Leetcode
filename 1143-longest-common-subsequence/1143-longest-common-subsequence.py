268f57481ad1cab46837d8d738f0ed55c147dff8
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-11-01 23:06:56 -0700

    Time: 503 ms (44.98%), Space: 42.7 MB (26.53%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,30 +1,17 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        '''
-        text1 = "abcde"
-        text2 = "ace" 
-        
-        '' a b c d e
-     ''  0 0 0 0 0 0 
-     a   0 1 1 1 1 1
-     c   0 1 1 2 2 2 
-     e   0 1 1 2 2 3
-     
-     if t1[j-1] == t2[i-1]:
-        DP[i][j] = 1 + DP[i-1][j-1]
-    else:
-        DP[i][j] = max(DP[i-1][j], DP[i][j-1])
-        
-        '''
-        
-        DP = [[0 for _ in range(len(text1)+1)] for _ in range(len(text2)+1)]
-        
-        for i in range(1, len(DP)):
-            for j in range(1, len(DP[0])):
-                if text1[j-1] == text2[i-1]:
-                    DP[i][j] = 1 + DP[i-1][j-1]
+        # i = text 1 length
+        # j = text 2 length
+        dp = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        # Base case for empty string already initialized
+        # Start at length 1
+        for i in range(1, len(text1)+1):
+            for j in range(1, len(text2)+1):
+                if text1[i-1] == text2[j-1]:
+                    # Chop off both suffixed
+                    dp[i][j] = 1  + dp[i-1][j-1]
                 else:
-                    DP[i][j] = max(DP[i-1][j], DP[i][j-1])
-                    
-        return DP[-1][-1]
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[-1][-1]
         
