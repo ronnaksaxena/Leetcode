379fbe357d55a7c78b88fa92ded491ec1c806657
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-11-01 22:35:02 -0700

    Time: 7 ms (19.79%), Space: 18.6 MB (14.39%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,16 +1,17 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+
+        @lru_cache
+        def dp(step):
+            if step <= 1:
+                return cost[0] if step == 0 else cost[1]
+            
+            return cost[step] + min(dp(step-1), dp(step-2))
+
+        return min(dp(len(cost)-1), dp(len(cost)-2))
+        
         '''
-        Time: O(n)
-        Space: O(n)
+        [10,15,20]
+               i
+
         '''
-        # Treat step i + 1 as top floor or goal state
-        DP = [0 for _ in range(len(cost)+1)]
-        
-        DP[0] = 0
-        DP[1] = 0
-        
-        for step in range(2, len(DP)):
-            DP[step] = min(cost[step-1] + DP[step-1], cost[step-2] + DP[step-2])
-        return DP[-1]
-        
