c3894f5727380192fbeb8ff290fb801a063dcf47
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-11-01 23:21:15 -0700

    Time: 672 ms (84.6%), Space: 253.2 MB (5.97%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,28 +1,18 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        '''
-        if s[i-1] == s[j-1]:
-            DP[i][j] = DP[i-1][j-1] + 1
-        else:
-            DP[i][j] = max(DP[i-1][j], DP[i][j-1])
-         '' b b b a b
-     ''  0  0 0 0 0 0
-     b   0  1 1 1 1 1
-     b   0
-     b   0
-     a   0
-     b   0
-        '''
-        n = len(s)
-        revS = s[::-1]
-        # +1 for empty stirng base case
-        DP = [[0 for _ in range(n+1)] for _ in range(n+1)]
-        
-        for i in range(1, len(DP)):
-            for j in range(1, len(DP[0])):
-                if s[i-1] == revS[j-1]:
-                    DP[i][j] = DP[i-1][j-1] + 1
-                else:
-                    DP[i][j] = max(DP[i-1][j], DP[i][j-1])
-        return DP[-1][-1]
+
+        @cache
+        def dp(l, r):
+            if l == r:
+                return 1
+            if (l+1) == r:
+                return 2 if s[l] == s[r] else 1
+
+            if s[l] == s[r]:
+                return 2 + dp(l+1, r-1)
+            else:
+                return max(dp(l+1, r), dp(l, r-1))
+
+        return dp(0, len(s)-1)
+            
         
