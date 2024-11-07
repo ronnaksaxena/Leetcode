95b99976a670b2a89949f8afe4d889ec8e8d7b30
Ronnak Saxena <63483386+ronnaksaxena@users.noreply.github.com>
2024-11-07 10:21:53 -0800

    Time: 3 ms (82.41%), Space: 17.7 MB (81.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        tailIndex = 0
-
+        idx = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[tailIndex], nums[i] = nums[i], nums[tailIndex]
-                tailIndex += 1
-
-
+                nums[i], nums[idx] = nums[idx], nums[i]
+                idx += 1
+        return None
         
